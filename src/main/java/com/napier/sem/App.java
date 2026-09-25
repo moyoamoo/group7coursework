b0b1eeeae3fe700c65b967f133e7ee718d834ee7
moyoamoo <moyo.amoo@hotmail.com>
2026-09-25 20:17:03 +0100

    CI changes

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,9 +16,9 @@ public class App
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "DevOps")
-                .append("year", "2024")
+        Document doc = new Document("name", "Moyo")
+                .append("class", "SEM")
+                .append("year", "2026")
                 .append("result", new Document("CW", 95).append("EX", 85));
         // Add document to collection
         collection.insertOne(doc);
